ea8293d42f956ab29ba9ccf13deb3d46bd5f6403
Brock Britton <brockbritton@outlook.com>
2024-06-04 13:10:35 -0700

    custom hook and authorization/stripe connection

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,36 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from "react";
+import useAuthClaim from "../hooks/customAuth";
 
 const Profile = () => {
-  const { user, isAuthenticated, getIdTokenClaims } = useAuth0();
-  const [role, setRole] = useState('');
-
-  useEffect(() => {
-    const fetchUserRole = async () => {
-      try {
-        const claims = await getIdTokenClaims();
-        console.log("ID Token Claims:", claims); // Log the complete ID token claims
-
-        const namespace = 'https://city-greens.com/'; // Use your custom namespace
-        const userRole = claims[`${namespace}role`];
-        setRole(userRole);
-      } catch (error) {
-        console.error('Error fetching user role:', error);
-      }
-    };
-
-    if (isAuthenticated) {
-      fetchUserRole();
-    }
-  }, [isAuthenticated, getIdTokenClaims]);
+  const { user, isAuthenticated } = useAuth0();
+  const role = useAuthClaim('role');
+  const onboarded = useAuthClaim('onboarded');
+  
 
   return (
-    isAuthenticated && (
+    isAuthenticated && (role === "vendor") && (
       <div className='profile'>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        {role && <p><strong>Role:</strong> {role}</p>}
+        <p><strong>Role:</strong> {role}</p>
       </div>
     )
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'react-redux'
 
 const Auth0Domain = import.meta.env.VITE_AUTH0_DOMAIN
 const Auth0ClientID = import.meta.env.VITE_AUTH0_CLIENTID
