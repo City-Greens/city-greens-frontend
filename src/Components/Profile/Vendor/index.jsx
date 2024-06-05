7ec307a55a80195b68b650300be149160a273e40
Isai Chaidez <isaichaidez@gmail.com>
2024-06-05 14:05:37 -0700

    created route to get products based of vendor stripe id

--- src/Components/Profile/Vendor/index.jsx
+++ src/Components/Profile/Vendor/index.jsx
@@ -10,6 +10,7 @@ const VendorProfile = () => {
   const { isAuthenticated } = useAuth0();
 
   const stripe_id = useAuthClaim("stripe_id");
+  console.log(account);
 
   const getAccount = async () => {
     try {
@@ -31,9 +32,49 @@ const VendorProfile = () => {
     }
   };
 
+  const getSingleProduct = async () => {
+    try {
+      const response = await fetch('http://localhost:4242/get-products', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id: stripe_id,
+        }),
+      });
+      const product = await response.json();
+      console.log(product);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // const addToDB = async () => {
+  //   try {
+  //     const response = await fetch('http://localhost:4242/vendor', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({
+  //         name: account.business_profile.name,
+  //         location: account.business_profile.address.city
+  //
+  //
+  //       }),
+  //     });
+  //     const account = await response.json();
+  //     setDetailsSubmitted(account.details_submitted);
+  //     setRequirements(account.requirements?.currently_due || []);
+  //     setAccount(account);
+  //   }
+  // });
+
   useEffect(() => {
     if (stripe_id && isAuthenticated) {
       getAccount();
+      getSingleProduct();
     }
   }, [stripe_id, isAuthenticated]);
 
@@ -62,6 +103,12 @@ const VendorProfile = () => {
         <>
           <h1>Details submitted, and no requirements!</h1>
           <p>Stripe Account ID: {stripe_id}</p>
+          <button>
+            <a href={`https://dashboard.stripe.com/test`} target="_blank" rel="noopener noreferrer">
+              Go to your dashboard!
+            </a>
+          </button>
+
         </>
       )}
     </div>
